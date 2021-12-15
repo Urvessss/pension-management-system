8ee195656fd5fd217a0ef9f7b9fb3adc7a64cc30
Urvesh <90395969+Urvessss@users.noreply.github.com>
2021-12-15 16:30:12 +0530

    c

--- src/components/BankData.js
+++ src/components/BankData.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { getBankById, addbankdetails, deletebankbyaccno,updatebankdetails } from '../redux/BankSlice';
 import axios from 'axios';
 
-    const BankData = props => {
+    const BankData =(props) => {
     const [accno, setAccno] = useState('');
     const dispatch = useDispatch();
     const bankDataFromStore = useSelector((state) => state.bank.bankState);
@@ -86,58 +86,10 @@ import axios from 'axios';
     }
 
   return (
-        <div className="container">
-            <h1 className="display-4 text-primary mt-3 mb-3" >Bank Component</h1>
-            <p>Fetch data from backend, store it in redux store and get it to component</p>
-
-            <div className="col-4 border border-light shadow p-3 mb-5 bg-white">
-                <p>Search Bank Account</p>
-                <form className="form form-group form-primary" onSubmit={submitGetBankById}>
-                    <input className="form-control mt-3" type="number" id="accno" name="accno" value={accno} onChange={handleBank} placeholder="Enter accno to search" />
-                    <input className="form-control mt-3 btn btn-primary" type="submit" value="Find Bank" />
-                </form>
-                <table className="table table-light table-striped ">
-                    <thead>
-                        <tr>
-                            <th>Accno </th>
-                            <th>BankName</th>
-                            <th>Branch</th>
-                            <th>AccoundHolderName</th>
-                            <th>ifcsCode</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{bankDataFromStore.accno}</td>
-                            <td>{bankDataFromStore.bankName}</td>
-                            <td>{bankDataFromStore.branch}</td>
-                            <td>{bankDataFromStore.accHolderName}</td>
-                            <td>{bankDataFromStore.ifscCode}</td>
-
-
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <p>--------------------</p>
-            <br></br>
-            <div className="container">
-
-                <div className="col-4 border border-light shadow p-3 mb-5 bg-white">
-                    <p>Remove Bank Account</p>
-                    <form className="form form-group form-primary" onSubmit={submitDeleteBankById}>
-                        <input className="form-control mt-4" type="number" id="accno" name="accno" value={accno} onChange={handleBank} placeholder="Enter accno to Delete" />
-                        <input className="form-control mt-4 btn btn-danger" type="submit" value="Remove Bank" />
-                    </form>
-
-
-                </div>
-             
-
-                <p>-----------------------------------------------------------------------------------------------------</p>
-                <div className="container">
+    <div className="container-fluid">
+    <h1 className="display-4 text-warning mt-3 mb-3" >Bank Component</h1>
+    <p>Fetch data from backend, store it in redux store and get it to component</p>
+    <div className="container">
                     <div className="col-4 border border-light shadow p-3 mb-5 bg-white">
                         <p>Add Bank Account</p>
                         
@@ -174,7 +126,60 @@ import axios from 'axios';
                         </table>
                     </div>
 
+                
+       
+
+            <div className="col-4 border border-light shadow p-3 mb-5 bg-white">
+                <p>Search Bank Account</p>
+                <form className="form form-group form-primary" onSubmit={submitGetBankById}>
+                    <input className="form-control mt-3" type="number" id="accno" name="accno" value={accno} onChange={handleBank} placeholder="Enter accno to search" />
+                    <input className="form-control mt-3 btn btn-primary" type="submit" value="Find Bank" />
+                </form>
+                <table className="table table-light table-striped ">
+                    <thead>
+                        <tr>
+                            <th>Accno </th>
+                            <th>BankName</th>
+                            <th>Branch</th>
+                            <th>AccoundHolderName</th>
+                            <th>ifcsCode</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{bankDataFromStore.accno}</td>
+                            <td>{bankDataFromStore.bankName}</td>
+                            <td>{bankDataFromStore.branch}</td>
+                            <td>{bankDataFromStore.accHolderName}</td>
+                            <td>{bankDataFromStore.ifscCode}</td>
+
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <p>--------------------</p>
+            <br></br>
+            <div className="container">
+
+                <div className="float- col-4 border border-light shadow p-3 mb-4 bg-white">
+                    <p>Remove Bank Account</p>
+                    <form className="form form-group form-primary" onSubmit={submitDeleteBankById}>
+                        <input className="form-control mt-4" type="number" id="accno" name="accno" value={accno} onChange={handleBank} placeholder="Enter accno to Delete" />
+                        <input className="form-control mt-4 btn btn-danger" type="submit" value="Remove Bank" />
+                    </form>
+
+
                 </div>
+             
+
+                <p>-----------------------------------------------------------------------------------------------------</p>
+                
+                
+                </div>
+
 
 <p>-------------------------------------------------</p>
                 {/* <div className="container">
@@ -214,9 +219,9 @@ import axios from 'axios';
                     </div>
 
                 </div> */}
+            
             </div>
-
-        </div>
+            </div>
 
     );
 }
